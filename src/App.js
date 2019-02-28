9cfbbdb4df5a598659eb4965e64eef767c9f0c86
David de Lusenet <david.delusenet@random.studio>
2019-02-28 16:55:38 +0100

    Add dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { Book, Group } from '@material-ui/icons';
 
+import Dashboard from './components/Dashboard/Dashboard';
 import PostCreate from './components/PostCreate/PostCreate';
 import PostEdit from './components/PostEdit/PostEdit';
 import PostList from './components/PostList/PostList';
@@ -11,7 +12,7 @@ import UserList from './components/UserList/UserList';
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={Book} />
         <Resource name="users" list={UserList} icon={Group} />
     </Admin>
